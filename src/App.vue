1c03e23f84509980926678d25a7c106875329f25
jiaming <743192023@qq.com>
2018-09-14 16:16:27 +0800

    add alert components

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,29 @@
   <div id="app">
     <canvas-b-g />
     <router-view/>
+
+    <confirm ref='confirm' />
+    <alert ref='alert' />
+    <tip ref='tip' />
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    aaaa () {
+      console.error(this.$confirm)
+    },
+    ...mapMutations(['setAppRootThis'])
+  },
+  created () {
+    const { setAppRootThis } = this
+
+    setAppRootThis(this)
+  }
 }
 </script>
 
