ef6c2ff35ef7cb2a9e97d49879e19dfec606807d
jiaming <743192023@qq.com>
2018-09-18 18:51:24 +0800

    config fight router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import main from '@/pages/main'
+import fight from '@/pages/fight'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '*',
+      redirect: { name: 'main' }
+    },
+    {
+      path: '/main',
       name: 'main',
       component: main
+    },
+    {
+      path: '/fight',
+      name: 'fight',
+      component: fight
     }
   ]
 })
