6ff21b5c393db347bef798e4307b153d456cad69
jiaming <743192023@qq.com>
2018-09-20 18:23:51 +0800

    game items json

---
+++ src/game/gameItems.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    name: 'i',
+    rotateAble: true,
+    rotate: 0,
+    points: [
+      [-1, 0],
+      [0, 0],
+      [1, 0],
+      [2, 0]
+    ]
+  },
+  {
+    name: 't',
+    rotateAble: true,
+    rotate: 0,
+    points: [
+      [0, -1],
+      [-1, 0],
+      [0, 0],
+      [1, 0]
+    ]
+  },
+  {
+    name: 'z',
+    rotateAble: true,
+    rotate: 0,
+    points: [
+      [-1, -1],
+      [0, -1],
+      [0, 0],
+      [1, 0]
+    ]
+  },
+  {
+    name: 'o',
+    rotateAble: false,
+    rotate: 0,
+    points: [
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [1, 1]
+    ]
+  },
+  {
+    name: 'l',
+    rotateAble: true,
+    rotate: 0,
+    points: [
+      [-1, -1],
+      [-1, 0],
+      [0, 0],
+      [1, 0]
+    ]
+  }
+]
