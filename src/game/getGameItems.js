202a8c027ac67b036bef128caa1d2e4888170d24
jiaming <743192023@qq.com>
2018-09-20 18:24:01 +0800

    get game items fun

---
+++ src/game/getGameItems.js
@@ -0,0 +1,27 @@
+import gameItems from './gameItems'
+
+import { rotatePoints } from '../plugins/canvasExtend'
+
+const allItemsNum = gameItems.length
+
+const { random, trunc } = Math
+
+function getArrayRandomIndex (length) {
+  return trunc(random() * length)
+}
+
+export default function (itemNum = 1) {
+  const items = []
+
+  for (let i = 0; i < itemNum; i++) {
+    const randomItem = gameItems[getArrayRandomIndex(allItemsNum)]
+
+    if (randomItem.rotateAble) randomItem.rotate = 90 * getArrayRandomIndex(4)
+
+    randomItem.points = rotatePoints(randomItem.points)
+
+    items.push(randomItem)
+  }
+
+  return items
+}
