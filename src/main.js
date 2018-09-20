2af688ffe121c5b193def9bad0088b4f56d9b58d
jiaming <743192023@qq.com>
2018-09-20 18:23:28 +0800

    optimization

--- src/main.js
+++ src/main.js
@@ -2,23 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+// router
 import router from './router'
+
+// Vuex store
+import store from './store'
 
 // Element-UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-// axiosExtend
-import './plugins/axiosExtend'
-
-// elementUI functions Extend
-import elementUIExtend from './plugins/elementUIExtend'
-
 // some global components
 import GlobalComponents from './components/index'
-
-// Vuex store
-import store from './store'
 
 // style
 import './assets/style/common.less'
@@ -27,16 +23,30 @@ import './assets/style/class.less'
 // icon
 import './assets/font/iconfont.css'
 
-// dialog plugins
-import dialogPlugins from './plugins/dialogExtend'
+// plugins
+import axiosPlugin from './plugins/axiosExtend'
+
+import elementUIPlugin from './plugins/elementUIExtend'
+
+import dialogPlugin from './plugins/dialogExtend'
+
+import canvasPlugin from './plugins/canvasExtend'
+
+import colorPlugin from './plugins/color'
 
 Vue.use(ElementUI)
 
-Vue.use(elementUIExtend)
-
 Vue.use(GlobalComponents)
 
-Vue.use(dialogPlugins)
+Vue.use(axiosPlugin)
+
+Vue.use(elementUIPlugin)
+
+Vue.use(dialogPlugin)
+
+Vue.use(canvasPlugin)
+
+Vue.use(colorPlugin)
 
 Vue.config.productionTip = false
 
