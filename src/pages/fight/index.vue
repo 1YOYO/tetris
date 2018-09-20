36ec15da406e1af372bf856d52b0251735716b5e
jiaming <743192023@qq.com>
2018-09-20 18:25:33 +0800

    optimization

--- src/pages/fight/index.vue
+++ src/pages/fight/index.vue
@@ -23,24 +23,225 @@
           00:06
         </div>
       </div>
+
       <div class="middle">
         <canvas ref="game" class="game"></canvas>
       </div>
+
       <div class="right">
-        <div class="next-tip">next tip</div>
-        <div class="current-info">current-info</div>
+        <div class="next-tip">
+          <canvas ref="next-tip-canvas" class="next-tip-canvas"></canvas>
+        </div>
+        <div class="current-info">
+          <div>Score <span>1190</span></div>
+          <div>Speed Level <span>2</span></div>
+          <div>View <span>0</span></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import getGameItems from '@/game/getGameItems'
+
 export default {
   name: 'Fight',
   data () {
-    return {}
+    return {
+      // game canvas ctx
+      gameCtx: {},
+      // game next tip canvas ctx
+      nextTipCtx: {},
+      // game canvas width & height
+      gameCanvasWH: {
+        width: 0,
+        height: 0
+      },
+      // game next tip canvas width & height
+      nextTipCanvasWH: {
+        width: 0,
+        height: 0
+      },
+      gameGridConfig: {
+        verticalNum: 10,
+        horizontalNum: 20,
+        lineWidth: 1,
+        lineColor: '#6ed4d3',
+        lineOpacity: 0.4,
+        gridWidth: 0,
+        gridHeight: 0
+      },
+      nextTipGridConfig: {
+        verticalNum: 12,
+        horizontalNum: 10,
+        lineWidth: 1,
+        lineColor: '#4bb7e4',
+        lineOpacity: 0.4,
+        gridWidth: 0,
+        gridHeight: 0
+      },
+      // game grid data
+      gameGridData: [],
+      // game next tip grid data
+      nextTipGridData: [],
+      // game data
+      gameData: [],
+      // next tip data
+      nextTipData: [],
+      // game status
+      gameStatus: true
+    }
   },
-  methods: {}
+  methods: {
+    /**
+     * @description             init game
+     * @return     {undefined}  no  return
+     */
+    initGame () {
+      const { initGameCanvas, initNextTipCanvas, getGridData } = this
+
+      initGameCanvas()
+      initNextTipCanvas()
+
+      const { gameCanvasWH, nextTipCanvasWH, gameGridConfig, nextTipGridConfig, gameGridData, nextTipGridData } = this
+
+      getGridData(gameCanvasWH, gameGridConfig, gameGridData)
+      getGridData(nextTipCanvasWH, nextTipGridConfig, nextTipGridData)
+
+      const { nextTipData, startGame } = this
+
+      nextTipData.push(...getGameItems(100))
+
+      startGame()
+    },
+    /**
+     * @description             init game canvas
+     * @return     {undefined}  no return
+     */
+    initGameCanvas () {
+      const { $refs, gameCanvasWH } = this
+
+      const canvas = $refs['game']
+
+      this.gameCtx = canvas.getContext('2d')
+
+      let [width, height] = []
+
+      gameCanvasWH.width = width = canvas.clientWidth
+      gameCanvasWH.height = height = canvas.clientHeight
+
+      canvas.setAttribute('width', `${width}px`)
+      canvas.setAttribute('height', `${height}px`)
+    },
+    /**
+     * @description             init game next tip canvas
+     * @return     {undefined}  no return
+     */
+    initNextTipCanvas () {
+      const { $refs, nextTipCanvasWH } = this
+
+      const canvas = $refs['next-tip-canvas']
+
+      this.nextTipCtx = canvas.getContext('2d')
+
+      let [width, height] = []
+
+      nextTipCanvasWH.width = width = canvas.clientWidth
+      nextTipCanvasWH.height = height = canvas.clientHeight
+
+      canvas.setAttribute('width', `${width}px`)
+      canvas.setAttribute('height', `${height}px`)
+    },
+    /**
+     * @description          get game grid line data
+     * @param      {object}  canvas width & height
+     * @param      {object}  grid config
+     * @param      {array}   store grid data's empty array
+     * @return     {Array}   line data
+     */
+    getGridData (canvasWH, gridConfig, gridDataArray) {
+      const { width, height } = canvasWH
+
+      const { verticalNum, horizontalNum, lineWidth } = gridConfig
+
+      const gridWidth = (width - lineWidth * (verticalNum - 1)) / verticalNum
+      const gridHeight = (height - lineWidth * (horizontalNum - 1)) / horizontalNum
+
+      gridConfig.gridWidth = gridWidth
+      gridConfig.gridHeight = gridHeight
+
+      // vertical line data
+      for (let i = 0; i < verticalNum; i++) {
+        const widthSpacing = gridWidth * (i + 1) + i * lineWidth
+
+        gridDataArray.push([
+          [widthSpacing, 0],
+          [widthSpacing, height]
+        ])
+      }
+
+      // horizontal line data
+      for (let i = 0; i < horizontalNum; i++) {
+        const heightSpacing = gridHeight * (i + 1) + i * lineWidth
+
+        gridDataArray.push([
+          [0, heightSpacing],
+          [width, heightSpacing]
+        ])
+      }
+    },
+    /**
+     * @description             draw grid
+     * @param      {object}     canvas ctx
+     * @param      {array}      grid line data
+     * @param      {object}     grid line config | lineWidth, lineColor, lineOpacity
+     * @return     {undefined}  no return
+     */
+    drawGrid (ctx, gridlineData, gridConfig) {
+      const { canvas: { drawLine }, color: { hexToRgb } } = this
+
+      const { lineWidth, lineColor, lineOpacity } = gridConfig
+
+      gridlineData.forEach(line => drawLine(ctx, line[0], line[1], lineWidth, hexToRgb(lineColor, lineOpacity)))
+    },
+    /**
+     * @description             draw game grid
+     * @return     {undefined}
+     */
+    drawGameGrid () {
+      const { drawGrid, gameCtx, gameGridData, gameGridConfig } = this
+
+      drawGrid(gameCtx, gameGridData, gameGridConfig)
+    },
+    /**
+     * @description             draw game next tip grid
+     * @return     {undefined}
+     */
+    drawNextTipGrid () {
+      const { drawGrid, nextTipCtx, nextTipGridData, nextTipGridConfig } = this
+
+      drawGrid(nextTipCtx, nextTipGridData, nextTipGridConfig)
+    },
+    /**
+     * @description             start game
+     * @return     {undefined}  no return
+     */
+    startGame () {
+      console.error(this.nextTipData)
+    },
+    drawGame () {
+
+    },
+    drawNextTip () {
+
+    }
+  },
+  mounted () {
+    const { initGame } = this
+
+    initGame()
+  }
 }
 </script>
 
@@ -136,12 +337,40 @@ export default {
       .next-tip, .current-info {
         box-sizing: border-box;
         margin-left: 10px;
-        box-shadow: 0 0 3px @deep-color;
+        box-shadow: 0 0 3px @addi-color;
         height: 395px;
       }
 
       .next-tip {
         margin-bottom: 10px;
+        overflow: hidden;
+
+        .next-tip-canvas {
+          width: 100%;
+          height: 100%;
+          box-shadow: 0 0 3px @addi-color;
+        }
+      }
+
+      .current-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        overflow: hidden;
+        font-size: 40px;
+
+        div {
+          height: 80px;
+          line-height: 80px;
+          text-indent: 20px;
+          .text-shadow-small(@addi-color);
+          box-shadow: 0 0 3px @addi-color;
+
+          span {
+            float: right;
+            margin-right: 20px;
+          }
+        }
       }
     }
 
