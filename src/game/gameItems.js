d2a947c37d3e669b9c084b1c25793b0a70f8a001
jiaming <743192023@qq.com>
2018-09-21 19:52:11 +0800

    add color opacity attribute

--- src/game/gameItems.js
+++ src/game/gameItems.js
@@ -3,6 +3,9 @@ export default [
     name: 'i',
     rotateAble: true,
     rotate: 0,
+    // color: '#6ed4d3',
+    color: '#645AF0',
+    opacity: '0.6',
     points: [
       [-1, 0],
       [0, 0],
@@ -14,6 +17,9 @@ export default [
     name: 't',
     rotateAble: true,
     rotate: 0,
+    // color: '#f5738f',
+    color: '#FEF06D',
+    opacity: '0.6',
     points: [
       [0, -1],
       [-1, 0],
@@ -25,6 +31,9 @@ export default [
     name: 'z',
     rotateAble: true,
     rotate: 0,
+    // color: '#4bb7e4',
+    color: '#F9263E',
+    opacity: '0.6',
     points: [
       [-1, -1],
       [0, -1],
@@ -36,6 +45,9 @@ export default [
     name: 'o',
     rotateAble: false,
     rotate: 0,
+    // color: '#ffdb65',
+    color: '#38DCAD',
+    opacity: '0.6',
     points: [
       [0, 0],
       [1, 0],
@@ -47,6 +59,9 @@ export default [
     name: 'l',
     rotateAble: true,
     rotate: 0,
+    // color: '#ff4f4f',
+    color: '#3BBDDC',
+    opacity: '0.6',
     points: [
       [-1, -1],
       [-1, 0],
